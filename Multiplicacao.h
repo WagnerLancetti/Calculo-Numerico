c18a7efcb3b0315bf702db93f06d9479dbd7c1be
WagnerLancetti <56642195+WagnerLancetti@users.noreply.github.com>
2019-11-28 15:50:52 -0300

    Regressão Linear

--- Multiplicacao.h
+++ Multiplicacao.h
@@ -1,6 +1,5 @@
-#define TAM 7
-float** GerarTransposta (float **Matriz_Original);
-float** Transposta_X_Original(float **Matriz_Transposta, float **Matriz_Original, float **resultado);
-float* Transposta_X_Funcao(float **Matriz_Transposta,float *y,float *resultado2);
+float** GerarTransposta (float **Matriz_Original, int TAM);
+float** Transposta_X_Original(float **Matriz_Transposta, float **Matriz_Original, float **resultado, int TAM);
+float* Transposta_X_Funcao(float **Matriz_Transposta,float *y,float *resultado2, int TAM);
 float* Inicializa_Vetor (float *y, int linhas);
 float** Inicializa_Matriz(float **Matriz, int linhas, int colunas);
