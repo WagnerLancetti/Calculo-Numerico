bd0bbf17ebf5756d60dadc4ba6939bc1cc9d210d
WagnerLancetti <56642195+WagnerLancetti@users.noreply.github.com>
2019-11-28 15:54:49 -0300

    Regressão Linear

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include "Multiplicacao.h"
 #include "SistemaLinear.h"
 
-#define TAM 7
 
 int main(){
+    int TAM = 6;
     float *y = NULL;
     y = Inicializa_Vetor(y,TAM);
     float **Matriz_Original = NULL;
@@ -15,12 +15,12 @@ int main(){
     float **Resultado1 = NULL;
     float *Resultado2 = NULL;
     FILE *input = fopen ("input.txt","r");
-    LeArquivo(input,y,Matriz_Original);
-    Matriz_Transposta = GerarTransposta(Matriz_Original);
+    LeArquivo(input,y,Matriz_Original,TAM);
+    Matriz_Transposta = GerarTransposta(Matriz_Original,TAM);
     Resultado1 = Inicializa_Matriz(Resultado1,6,6);
     Resultado2 = Inicializa_Vetor(Resultado2,6);
-    Resultado1 = Transposta_X_Original(Matriz_Transposta,Matriz_Original,Resultado1);
-    Resultado2 = Transposta_X_Funcao(Matriz_Transposta,y,Resultado2);
+    Resultado1 = Transposta_X_Original(Matriz_Transposta,Matriz_Original,Resultado1,TAM);
+    Resultado2 = Transposta_X_Funcao(Matriz_Transposta,y,Resultado2,TAM);
     Resultado2 = ResolveSistemaLinear(Resultado1,Resultado2);
     printf ("\nIntercept = %f",Resultado2[0]);
     printf ("\nð1 = %f",Resultado2[1]);
@@ -29,9 +29,10 @@ int main(){
     printf ("\nð4 = %f",Resultado2[4]);
     printf ("\nð5 = %f\n\n",Resultado2[5]);
     free (Resultado1);
-    free(Resultado2);
     free(Matriz_Transposta);
+
     free(Matriz_Original);
+    free(Resultado2);
     free(y);
     fclose (input);
 }
