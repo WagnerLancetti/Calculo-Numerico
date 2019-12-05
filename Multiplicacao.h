777fb9e67dc6396f36ad062d0c5ba655ffc9c7b2
WagnerLancetti <56642195+WagnerLancetti@users.noreply.github.com>
2019-12-05 11:09:36 -0300

    Delete Multiplicacao.h

--- Multiplicacao.h
+++
@@ -1,5 +0,0 @@
-float** GerarTransposta (float **Matriz_Original, int TAM);
-float** Transposta_X_Original(float **Matriz_Transposta, float **Matriz_Original, float **resultado, int TAM);
-float* Transposta_X_Funcao(float **Matriz_Transposta,float *y,float *resultado2, int TAM);
-float* Inicializa_Vetor (float *y, int linhas);
-float** Inicializa_Matriz(float **Matriz, int linhas, int colunas);
